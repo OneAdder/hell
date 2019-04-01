66059daaeaf61b652d872c0e7bee3ee119f7d7b4
Michael Voronov <mikivo@list.ru>
2019-04-01 10:19:34 +0300

    Update h3.R

--- h3.R
+++ h3.R
@@ -1,7 +1,7 @@
 data = read.csv('https://raw.githubusercontent.com/agricolamz/2019_data_analysis_for_linguists/master/data/students/OneAdder/hw3_Bayesian_inference/hw3_phoible.csv')
  
 #1
-print(prod(dnorm(data$consonants, mean = 22, sd = 6)))
+print(prod(dnorm(data$consonants, mean=22, sd=6)))
 
 #2
 data = read.csv('https://raw.githubusercontent.com/agricolamz/2019_data_analysis_for_linguists/master/data/letter_frequency.csv')
